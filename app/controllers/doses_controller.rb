255b77ea06778ef7240b8a53e6429cc2f8e60abd
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-18 16:33:26 +0000

    Rake completely passed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,19 @@
 class DosesController < ApplicationController
 
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:new, :create,]
   end
   resources :doses, only: [:destroy]
 end
