adeeb35c6a3c66a9489b3b60938c995074789d26
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-18 16:23:42 +0000

    Cocktail app functioning, no styling.

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,20 @@
+class DosesController < ApplicationController
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
