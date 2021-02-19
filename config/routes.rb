e016263bc6522c438974bbb94ca321be38dd1f27
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-19 16:23:50 +0000

    Ready to push to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
